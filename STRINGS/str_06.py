068481870db5e1e530260af6d1ca3c5ebf598b75
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-02-09 22:10:14 +0530

    Create str_06.py

---
+++ STRINGS/str_06.py
@@ -0,0 +1,7 @@
+# Count how many times a substring appears in a string.
+
+a = input("enter the string:")
+b = input("enter the substring :")
+
+count = a.count(b)  # count of substring b in string a
+print("the count of substring is:",count)
