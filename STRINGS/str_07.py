d955599cfc8eb28a9404636e9e125f4616171dcb
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-02-10 17:26:13 +0530

    Create str_07.py

---
+++ STRINGS/str_07.py
@@ -0,0 +1,8 @@
+# Convert a string to uppercase, lowercase, and title case
+a = input("enter  a string=")
+
+print("uppercase:",a.upper())
+print("lowercase:", a.lower())
+print("titlecase:",a.title())
+
+
