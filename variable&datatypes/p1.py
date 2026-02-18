ea97649cc1341fec32e526f73790c41fa7047b76
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-02-18 21:16:43 +0530

    Create p1.py

---
+++ variable&datatypes/p1.py
@@ -0,0 +1,7 @@
+# Create a variable to store your name, age, and height. Print them in one formatted sentence.
+
+name = "palak"
+age = 20
+height = 5.7
+
+print(f" my name is {name} and age is {age} and height is {height}")
