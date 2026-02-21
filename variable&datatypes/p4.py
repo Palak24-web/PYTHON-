c8996c49a488af1e9a725bbbd5e7b83899f0445b
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-02-21 21:31:32 +0530

    Create p4.py

---
+++ variable&datatypes/p4.py
@@ -0,0 +1,13 @@
+# Take user input of two numbers and print their sum, product, and average.
+
+num1=int(input("enter 1st number:"))
+num2=int(input("enter 2nd number:"))
+
+sum=num1+num2
+product = num1 * num2
+average = sum / 2
+
+print(sum)
+print(product)
+print(average)
+
