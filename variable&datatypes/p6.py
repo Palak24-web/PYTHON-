d90a023c2c4d9a3c2c47add46e80348d9631c5c8
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-02-23 00:09:30 +0530

    Create p6.py

---
+++ variable&datatypes/p6.py
@@ -0,0 +1,11 @@
+#  Create variables for marks in 5 subjects and compute percentage.
+
+a=45
+b=75
+c=40
+d=60
+e=32
+
+total = a+b+c+d+e
+percentage = (total/500)*100
+print("percentage is :",percentage)
