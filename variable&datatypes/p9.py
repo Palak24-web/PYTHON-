2bcd8ba84fe6b0b932772eff994533d56ad7ced2
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-02-26 01:38:29 +0530

    Create p9.py

---
+++ variable&datatypes/p9.py
@@ -0,0 +1,11 @@
+# Write a program that takes a number and shows whether it is even or odd.
+
+
+a = int(input("enter a number:"))
+
+if a %2==0:
+    print("even")
+    
+else:
+    print("odd")
+    
