508ce1fdaed43e504ddcd383fed3df2b3b1656ce
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-03-16 01:16:27 +0530

    Create 2.py

---
+++ sets&dictionary/2.py
@@ -0,0 +1,10 @@
+words={
+    "apple": "shew",
+    "banana": "kela",
+    "brain": "dimaag"
+
+}
+
+word = input("enter the word u want the meaning of:")
+
+print(words.get(word))  #or we can write print(words[word])
