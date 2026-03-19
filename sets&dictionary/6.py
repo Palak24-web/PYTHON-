85605e2ae4b4a214ababea2ccc6159064c45d13c
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-03-20 00:50:09 +0530

    Create 6.py

---
+++ sets&dictionary/6.py
@@ -0,0 +1,9 @@
+g1 = {3,65,77}
+g2 = {90,43,3,1}
+
+print(g1.union(g2)) # union of two sets
+print(g1.intersection(g2)) # intersection of 2 sets
+ 
+
+print(g1.issubset(g2))   # True
+print(g1.issuperset(g2))
