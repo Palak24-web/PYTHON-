e3abd95c00e31f8a3d4a3f338066b02ce62eb611
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-03-22 00:54:41 +0530

    Create 1st.py

---
+++ conditional/1st.py
@@ -0,0 +1,13 @@
+a= int(input("enter your age:"))
+
+
+if(a>18):
+    print("your are above the age of consent")
+
+elif(a<0):
+        print("invalid age")
+
+else:
+        print("your below the age of consent")
+
+        print("end of program")
