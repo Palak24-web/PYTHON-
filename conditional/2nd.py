c459eb7e1f64cf205f31d1d5508b582619eabbe4
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-03-23 00:18:13 +0530

    Create 2nd.py

---
+++ conditional/2nd.py
@@ -0,0 +1,19 @@
+a= int(input("enter ur age:"))
+
+#If statement no 1:
+if(a%2==0):
+    print("a is even")
+
+#end of if staement no 1
+#If statement no 2:
+if(a>18):
+    print("your are above the age of consent")
+
+elif(a<0):
+        print("invalid age")
+
+else:
+        print("your below the age of consent")
+
+        print("end of program")
+        # end of if statemnt no 2
