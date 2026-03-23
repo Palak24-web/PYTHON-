c2f50dc3e5f20234d37827bbe1538a5988898102
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-03-24 01:01:04 +0530

    Create 3rd.py

---
+++ conditional/3rd.py
@@ -0,0 +1,17 @@
+a=int(input("enter number 1:"))
+b=int(input("enter number 2:"))
+c=int(input("enter number 3:"))
+d=int(input("enter number 4:"))
+
+if(a>b and a>c and a>d):
+    print("a is greater")
+
+elif(b>a and b>c and b>d):
+     print("b is greater")
+
+elif(c>a and c>b and c>d):
+  print("c is greater")
+
+else:
+ print("d is greater")
+    
