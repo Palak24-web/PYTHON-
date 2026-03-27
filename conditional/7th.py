f91fc4f96f874d4734cccc1d0624380f35bda00b
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-03-28 00:57:45 +0530

    Create 7th.py

---
+++ conditional/7th.py
@@ -0,0 +1,8 @@
+P={"nidhi","manush","geeta","pyaari"}
+
+name = input("enter ur name=")
+
+if(name in P):
+    print("ur name is present in the list")
+else:
+        print("ur name is not in a list")
