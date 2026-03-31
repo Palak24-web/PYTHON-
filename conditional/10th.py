8ff9a1a2fa49c05fa5b0c26ccc371405865bbce5
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-03-31 10:23:12 +0530

    Create 10th.py

---
+++ conditional/10th.py
@@ -0,0 +1,11 @@
+# Check if a number is positive, negative, or zero.
+
+a = int(input("enter a number:"))
+
+if a>0:
+    print("positive")
+elif a<0:
+    print("negative")
+else:
+    print("zero")
+    
