b9775824edf20293966f3be7adebad4fef004633
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-04-03 09:58:58 +0530

    Create 3apr.py

---
+++ conditional/3apr.py
@@ -0,0 +1,9 @@
+# Check if a number is divisible by both 3 and 5.
+
+a=int(input("enter a  number"))
+
+if a%3==0 & a%5==0:
+    print("divisible by 3 and 5")
+else:
+    print("not divisible")
+    
