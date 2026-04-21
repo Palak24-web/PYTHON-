2ea83122a7977aae64d739fda06a28707c571f76
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-04-21 09:55:23 +0530

    Create 2ndwhile.py

---
+++ loops/2ndwhile.py
@@ -0,0 +1,9 @@
+for i in range(26):
+    if(i==19):
+        break #exit the loop when i is 19
+    print(i)
+
+    for j in range(30):
+        if(j==20):
+            continue # skip the iteration means skip 20 and continue with next value
+        print(j)
