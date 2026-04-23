0160d2154126251a317f30a6bf47d3321a5aff92
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-04-24 00:06:22 +0530

    Create 24apr.py

---
+++ loops/24apr.py
@@ -0,0 +1,13 @@
+list={1,"veena","kangana",6.7,False}
+
+for i in list:
+    print(i)
+
+
+    t=(5,66,"hello",0)
+    for j in t:
+        print(j)
+
+        q="radha"
+        for k in q:
+            print(k)
