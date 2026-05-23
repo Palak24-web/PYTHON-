223c9f00a44d81873a6b40129ce2c54d63169899
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-05-23 09:15:07 +0530

    Create sat.py

---
+++ oops/sat.py
@@ -0,0 +1,19 @@
+#function definition
+def avg():
+
+  a=int(input("enter a number:"))
+  b=int(input("enter a number:"))
+  c=int(input("enter a number:"))
+
+  average=(a + b + c)/3
+  print("the average is:",average)
+
+
+#function calls
+avg()
+print("done")
+avg()
+print("ok again done")
+avg()
+print("final done")
+
