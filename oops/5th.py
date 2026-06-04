28a6157225a7537f49ca33f0ea1eeeeb577751aa
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-06-05 00:33:04 +0530

    Create 5th.py

---
+++ oops/5th.py
@@ -0,0 +1,12 @@
+class bhoooott:
+    name = "hawwww😱😱"
+    
+    @classmethod
+    def changename(cls,name):
+      cls.name = name
+       
+a = bhoooott()
+a.changename("bhoot voot kn hota🤣")  
+print(a.name)
+print(bhoooott.name)
+      
