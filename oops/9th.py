792afc90ebf402064859ef14c9811523c545b3c3
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-06-09 00:15:32 +0530

    Create 9th.py

---
+++ oops/9th.py
@@ -0,0 +1,14 @@
+class college:
+    
+    def __init__(self,fullname,marks):
+        self.fullname = fullname
+        self.marks = marks
+        print("adding new students database:")
+        
+s1 = college("shreeji",96)
+print(s1.fullname,s1.marks)    
+
+s2 = college("kfjs💕",56)
+print(s2.fullname,s2.marks)    
+        
+        
