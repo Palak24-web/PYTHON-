bb58e97d63cd1ea6bbc5181230e7f1c2c8d7072b
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-06-12 09:59:04 +0530

    Create 12th.py

---
+++ oops/12th.py
@@ -0,0 +1,14 @@
+class love:
+     love = "i love u baby 💕"
+    
+class tatti:
+    tatti = " hut tuutty 💩saalaa🤣🤣"
+    
+class hehe(love,tatti):
+     hehe = "hehe maza aayaaa😏"
+    
+c1 = hehe()
+
+print(c1.love)
+print(c1.tatti)
+print(c1.hehe)            
