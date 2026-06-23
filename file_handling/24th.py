4a045ba025c85b26444314c795ee2f8871baf4f1
Palak Vishwakarma <palakvishwakarma1212@gmail.com>
2026-06-24 00:53:30 +0530

    Create 24th.py

---
+++ file_handling/24th.py
@@ -0,0 +1,7 @@
+st = " hey radha u r so beautiful "
+
+f = open("myfile.txt","a") # a is for append mode means jitni baar ham file chalayge utni baar myfile m data add hota jayga uske last m 
+
+f.write(st)
+
+f.close()
